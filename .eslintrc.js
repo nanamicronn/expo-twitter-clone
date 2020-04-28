7ed34bdf14d172f982989ecd0cd142c094f14ced
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2020-04-28 22:37:09 +0900

    [add] add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  env: {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "airbnb",
+  ],
+  globals: {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  parserOptions: {
+    "ecmaFeatures": {
+    "jsx": true
+  },
+  "ecmaVersion": 2018,
+  "sourceType": "module"
+  },
+  plugins: [
+    "react",
+    "@typescript-eslint",
+    "jsx-a11y",
+    "import",
+    "eslint-plugin-prettier",
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".tsx", "ts"] }],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".json",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
+  }
+};
