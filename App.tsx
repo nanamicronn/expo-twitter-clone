b4ef409b4f8b996c7fb6a0e68581c777e93971dc
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2020-04-28 22:42:44 +0900

    [update] ReactNavigation追加・Appを関数コンポーネントに変更

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -17,3 +18,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
